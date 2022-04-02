f5e3f75077164c52e1c32c4f205890f8286761c7
zaurusman <yotamtsa@gmail.com>
2022-04-02 13:37:31 +0300

    fixed, now prints 234124

--- print_number.c
+++ print_number.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
-		printf("%d", course_number[i]);
-	}
-	printf("\n");
-	return 0;
+        int course_number[6] = {2, 3, 4, 1, 2, 4};
+        for(int i = 0; i < 6; i++) {
+            printf("%d", course_number[i]);
+        }
+        printf("\n");
+        return 0;
 }
